fa620ada6e09adcdb7b20d84552c40af2c2766ce
Gene J.B. <sterr@list.ru>
2026-05-12 22:39:24 +0000

    Configurator error Exp Rom load

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STRING "v1.1.0-60-g88633c2-dirty"
-#define VERSION_SHORT "v1.1.0-60-g88633c2"
+#define VERSION_STRING "v1.1.0-61-g660ae06-dirty"
+#define VERSION_SHORT "v1.1.0-61-g660ae06"
 #define VERSION_IS_DIRTY 1
 
 #endif // VERSION_H
